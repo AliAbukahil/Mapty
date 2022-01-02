eb4f5d36c3d2c5faf4beecb057f3f267a943f5dc
ali.abukahil <ali.abukahil@gmail.com>
2022-01-02 18:27:13 +0100

    Managing Workout Data Creating Classes

--- script.js
+++ script.js
@@ -11,6 +11,57 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+  // date and id are fields
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+// child class
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    // need to call super() method with the common arguments coming from the parent class (Workout)
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  // creating a method for calculating the pace
+  calcPace() {
+    // min in km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+// child class
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    // need to call super() method with the common arguments coming from the parent class (Workout)
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  // creating a method for calculating the speed
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// for experiment only!
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cyling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cyling1);
+
+//////////////////////////////////
+// Application Architecture
 class App {
   // Private instance properties
   #map;
